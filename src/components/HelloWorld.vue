8961b23cc0cc808da232208a3c94335a960eb718
Snowren1123 <39525249+snowren1123@users.noreply.github.com>
2020-03-12 00:35:52 +0800

    first-commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="hello">
+    <h1>登录</h1>
+    <el-form
+      :model="dynamicValidateForm"
+      ref="dynamicValidateForm"
+      label-width="100px"
+      class="demo-dynamic center-set"
+    >
+      <el-form-item
+        prop="email"
+        label="邮箱"
+        :rules="[
+      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
+      { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+    ]"
+      >
+        <el-input v-model="dynamicValidateForm.email"></el-input>
+      </el-form-item>
+      <el-form-item
+        prop="password"
+        label="密码"
+        :rules="{
+      required: true, message: '密码不能为空', trigger: 'blur'
+    }"
+      >
+        <el-input v-model="dynamicValidateForm.password" show-password></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('dynamicValidateForm')">确认</el-button>
+        <el-button @click="addDomain">注册</el-button>
+        <el-button @click="resetForm('dynamicValidateForm')">忘记密码</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      dynamicValidateForm: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert('登录成功!')
+        } else {
+          console.log('登录失败!')
+          return false
+        }
+      })
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields()
+    },
+    addDomain() {
+      alert('注册!')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.center-set {
+  width: 40%;
+  margin-left: 27%;
+}
+</style>
