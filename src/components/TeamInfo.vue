8d0c3c908941eaa64c715d315a6055f56fa5826b
Snowren1123 <39525249+snowren1123@users.noreply.github.com>
2020-03-29 00:52:02 +0800

    添加获取项目成员信息

--- src/components/TeamInfo.vue
+++ src/components/TeamInfo.vue
@@ -1,103 +1,74 @@
 <template>
   <el-main>
     <el-row :gutter="20">
-      <el-col :span="8">
-        <el-card shadow="always">
-          <div slot="header" class="clearfix">
-            <span>项目经理</span>
-          </div>
-          <div class="text">
-            <p>
-              <b>姓名：</b>
-              <span>{{personInfo.name}}</span>
-            </p>
-            <p>
-              <b>邮箱：</b>
-              <span>{{personInfo.email}}</span>
-            </p>
-            <p>
-              <b>手机号：</b>
-              <span>{{personInfo.mobile}}</span>
-            </p>
-            <p>
-              <b>地址：</b>
-              <span>{{personInfo.address}}</span>
-            </p>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card shadow="hover">
-          <div slot="header" class="clearfix">
-            <span>开发人员</span>
-          </div>
-          <div class="text">
-            <p>
-              <b>姓名：</b>
-              <span>{{personInfo.name}}</span>
-            </p>
-            <p>
-              <b>邮箱：</b>
-              <span>{{personInfo.email}}</span>
-            </p>
-            <p>
-              <b>手机号：</b>
-              <span>{{personInfo.mobile}}</span>
-            </p>
-            <p>
-              <b>地址：</b>
-              <span>{{personInfo.address}}</span>
-            </p>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card shadow="hover">
-          <div slot="header" class="clearfix">
-            <span>测试人员</span>
-          </div>
-          <div class="text">
-            <p>
-              <b>姓名：</b>
-              <span>{{personInfo.name}}</span>
-            </p>
-            <p>
-              <b>邮箱：</b>
-              <span>{{personInfo.email}}</span>
-            </p>
-            <p>
-              <b>手机号：</b>
-              <span>{{personInfo.mobile}}</span>
-            </p>
-            <p>
-              <b>地址：</b>
-              <span>{{personInfo.address}}</span>
-            </p>
-          </div>
-        </el-card>
-      </el-col>
+      <template v-for="member in members">
+        <el-col :span="8" v-bind:key="member.index">
+          <el-card shadow="always">
+            <div slot="header" class="clearfix">
+              <span>{{ member.role }}</span>
+            </div>
+            <div class="text">
+              <p>
+                <b>ID：</b>
+                <span>{{ member.employeeId }}</span>
+              </p>
+              <p>
+                <b>邮箱：</b>
+                <span>{{ member.email }}</span>
+              </p>
+              <p>
+                <b>手机号：</b>
+                <span>{{ member.mobile }}</span>
+              </p>
+              <p>
+                <b>地址：</b>
+                <span>{{ member.address }}</span>
+              </p>
+            </div>
+          </el-card>
+        </el-col>
+      </template>
     </el-row>
   </el-main>
 </template>
 
 <script>
+import axios from "axios";
+import qs from "qs";
+import { mapState } from "vuex";
+
 export default {
   data() {
     return {
-      personInfo: {
-        name: "ocean",
-        email: "18912341312@qq.com",
-        mobile: "18918918189",
-        address: "ecnu"
-      }
+      members: []
     };
+  },
+
+  created() {
+    this.getProjectMembers();
+  },
+
+  computed: {
+    ...mapState(["projectBasicId"])
+  },
+
+  methods: {
+    getProjectMembers() {
+      axios.get("/api/member/" + this.projectBasicId).then(response => {
+        if (response.data.code === 0) {
+          console.log(response.data);
+          this.members = response.data.data;
+        }
+      });
+      // 需添加获取员工信息
+    }
   }
 };
 </script>
 
 <style>
 .el-card__body {
-    padding-top: 10px;
+  padding-top: 10px;
 }
 .el-row {
   margin-bottom: 20px;
