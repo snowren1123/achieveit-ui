9f60b1100a53e8cd57c3ccd0f6ba98c2e19bb8ff
OneTwe <onetwe@163.com>
2020-04-07 18:50:39 +0800

    22th submission by lxq
    请求拦截器、响应拦截器

--- src/main.js
+++ src/main.js
@@ -17,10 +17,35 @@ Vue.config.productionTip = false
 axios.defaults.withCredentials = true;
 //axios.defaults.baseURL = (process.env.NODE_ENV === 'development') ? '/api' : process.env.BASE_API;
 //axios.defaults.baseURL = '/api'
-axios.interceptors.request.use(config => {
-  config.headers.Authorization = "Bearer " + Cookie.get("token")
-  return config // 返回请求对象
-})
+
+// http request 拦截器
+axios.interceptors.request.use(
+  config => {
+    const token = Cookie.get('token')
+    if (token) { // 判断是否存在token，如果存在的话，则每个http header都加上token
+      config.headers.Authorization = "Bearer " + Cookie.get("token")  //请求头加上token
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  })
+
+// http response 拦截器
+axios.interceptors.response.use(
+  response => {
+    //拦截响应，做统一处理 
+    if (!Cookie.get('token') && response.data.data) {
+      router.replace({
+        path: '/login'
+      })
+    }
+    return response
+  },
+  //接口错误状态处理，也就是说无响应时的处理
+  error => {
+    return Promise.reject(error.response.status) // 返回接口返回的错误信息
+  })
 
 /* eslint-disable no-new */
 new Vue({
